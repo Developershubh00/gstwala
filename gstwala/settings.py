a88d3423e51832e6cd9081a7f552775c5b329a04
Aakash <aakashverma51805@gmail.com>
2024-09-07 16:43:30 +0530

    hosting on vercel

--- gstwala/settings.py
+++ gstwala/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-82uz2*k0m^)4u=x4=kh*w@d392wd%^#=&7@@x@!_nb94qv0ql@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app' , '.now.sh','127.0.0.1']
 
 
 # Application definition
